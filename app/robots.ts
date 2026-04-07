2e1e8613d164e30edab207daa66e1465acac9d9e
okuwabara07-gif <o.kuwabara07@gmail.com>
2026-04-08 01:31:03 +0900

    fix: app/robots.ts削除（static export非対応のため）

--- app/robots.ts
+++
@@ -1,8 +0,0 @@
-export const dynamic = 'force-static'
-
-export default function robots() {
-  return {
-    rules: { userAgent: '*', allow: '/' },
-    sitemap: 'https://okuwabara07-gif.github.io/haircolor-lab/sitemap.xml',
-  }
-}
