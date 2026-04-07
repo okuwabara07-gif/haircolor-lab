d0f081135f86b418ccea3b0e540f2ff4d4e0216d
okuwabara07-gif <o.kuwabara07@gmail.com>
2026-04-08 01:55:18 +0900

    fix: .nextキャッシュ削除 .next/dev/static/chunks/haircolor-lab_0rqeker._.js

--- .next/dev/static/chunks/haircolor-lab_0rqeker._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/[turbopack]_browser_dev_hmr-client_hmr-client_ts_13nhfj-._.js","static/chunks/04pv_next_dist_compiled_next-devtools_index_0n32v.l.js","static/chunks/04pv_next_dist_compiled_react-dom_13qn7qt._.js","static/chunks/04pv_next_dist_compiled_react-server-dom-turbopack_0.l5ydu._.js","static/chunks/04pv_next_dist_compiled_11.ewqm._.js","static/chunks/04pv_next_dist_client_09te1hz._.js","static/chunks/04pv_next_dist_0nrvb0r._.js","static/chunks/04pv_@swc_helpers_cjs_0du~rux._.js"],
-    source: "entry"
-});
