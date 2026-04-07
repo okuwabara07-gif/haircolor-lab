f0804f5063aa526f558e2de5b335079f2eaf6f63
okuwabara07-gif <o.kuwabara07@gmail.com>
2026-04-07 21:46:53 +0900

    fix: assetPrefix設定

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+import type { NextConfig } from 'next'
+const nextConfig: NextConfig = {
   output: 'export',
   images: { unoptimized: true },
   trailingSlash: true,
+  basePath: '',
+  assetPrefix: '',
 }
-module.exports = nextConfig
+export default nextConfig
