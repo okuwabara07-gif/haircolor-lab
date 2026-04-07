cacd214d1c9d80a90060a1fb631f8a34e3953cf9
okuwabara07-gif <o.kuwabara07@gmail.com>
2026-04-08 01:48:14 +0900

    fix: next-sitemap generateRobotsTxt無効化

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,11 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: 'https://haircolor-lab.vercel.app',
-  generateRobotsTxt: true,
-  robotsTxtOptions: {
-    additionalSitemaps: ['https://haircolor-lab.vercel.app/sitemap.xml'],
-  },
-  changefreq: 'daily',
-  priority: 0.7,
-  sitemapSize: 5000,
+  siteUrl: 'https://okuwabara07-gif.github.io/haircolor-lab',
+  generateRobotsTxt: false,
+  outDir: './out',
 }
